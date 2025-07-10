690026cd8d0294418e67ed76c17daf5c97817531
Shennngg <sherrylbabaran014@gmail.com>
2025-07-10 21:33:43 +0800

    summary

--- back_end/customer/dashboard.php
+++ back_end/customer/dashboard.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['fullname'])) {
 <head>
     <title>Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
             background-color: #f5f5dc;
--- back_end/customer/forgot_password.php
+++ back_end/customer/forgot_password.php
@@ -52,7 +52,7 @@ if (
     <title>Forgot Password</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- ✅ Bootstrap 5 CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
             background: linear-gradient(135deg, #000 50%, #f5f5dc 50%);
